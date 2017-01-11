c2e9071ef324b47622f6249dde467d2e0ece7883
gaunthan <gaunthanhuang@foxmail.com>
2017-01-11 10:15:53 +0800

    Refactoring, add more clear prompt.

--- shell-env-settings/PS1-setting/PS1-cfg.sh
+++ shell-env-settings/PS1-setting/PS1-cfg.sh
@@ -6,22 +6,30 @@ profile="${HOME}/.bashrc"
 # Read configuration of PS1.
 PS1=$(cat $ps1_cfg)
 
-echo "PS1 was configured.Would you like to continue?"
-echo "If continue, We will comment your previous configrations."
-echo -n "Enter \"yes\" or \"no\": "
+# Check if configured.
+grep "^PS1=" $profile --silent
+if [ $? = 0 ]; then
+	echo "PS1 was configured.Would you like to continue?"
+	echo "If continue, We will comment your previous configrations."
+	echo -n "Enter \"yes\" or \"no\": "
 
-read ans
-if [ $ans = "yes" ]; then
-	# Comment the existent configurations.
-	sed -i -s 's/^[^#]*PS1=/# &/' $profile
-
-	# Add it.
-	echo "PS1=\"${PS1}\"" >> $profile
-	
-	# Reload profile.
-	. $profile
-
-	echo "Operation done."
-else
-	echo "Operation aborted."
+	read ans
+	if [ $ans = "no" ]; then
+		echo "Operation aborted."
+		exit
+	else
+		echo "Commenting your previous configurations..."
+		# Comment the existent configurations.
+		sed -i -s 's/^[^#]*PS1=/# &/' $profile
+	fi
 fi
+
+echo "Configuring PS1..."
+
+# Add it.
+echo "PS1=\"${PS1}\"" >> $profile
+	
+# Reload profile.
+. $profile
+
+echo "Operation done."
